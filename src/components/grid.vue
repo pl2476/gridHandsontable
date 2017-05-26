61b798cebea1f675a972c8c010cdff99cf942647
Light Pen <Light Pen>
2017-05-26 17:20:05 +0800

    changecss

--- src/components/grid.vue
+++ src/components/grid.vue
@@ -6,13 +6,14 @@
       Head to
       <a href="https://handsontable.com" target="_blank">handsontable.com</a> to learn more about Handsontable.
     </p>
-    <div id="example"></div>
+    <div id="example" ></div>
   </div>
 </template>
 
 <script>
 import Handsontable from 'handsontable'
-
+import $ from 'jquery'
+console.log($)
 
 
 export default {
@@ -26,13 +27,14 @@ export default {
   },
   mounted() {
     this.populateHot();
+    this.changeCss();
   },
   methods: {
     populateHot: function () {
       var data = function () {
         return Handsontable.helper.createSpreadsheetData(100, 10);
       };
-      console.log(data());
+      // console.log(data());
 
       var container = document.getElementById('example');
       var hot = new Handsontable(container, {
@@ -41,15 +43,48 @@ export default {
         minSpareRows: 1,
         rowHeaders: true,
         colHeaders: true,
-        contextMenu: true
+        contextMenu: true,
+        // currentRowClassName:
+        fixedRowsTop : 1
       });
+    },
+    changeCss:function(){
+      // $('.handsontable thead th').css('line-height':'30px','color':'#276');
+      $('.handsontable thead th .relative').css('color','#276');
+      $('.handsontable thead th .relative').css('line-height','50px');
+      // $('.handsontable thead th .relative').css('line-height':'10px','color':'red');
+      $('.handsontable thead th').addClass('pl');
     }
   }
 }
 </script>
-<style>
+<style scoped>
+
 @import '../assets/handsontable/handsontable.full.min.css';
-@import '../assets/handsontable/main.css';
+/*@import '../assets/handsontable/main.css';*/
+/*.handsontable thead th .relative{
+  background-color: #276 !important; 
+}
+.handsontable thead th .relative{
+  font-size: 20px;
+}
+.handsontable thead {
+  background-color: #276;
+}
+.wtBorder {
+  background-color: #F00!important;
+}
+.ht_master tr td {
+  background-color: #F00;
+}
+.ht_master tr > td:nth-of-type(odd) {
+  background-color: #f00;
+}*/
+.handsontable thead th .pl{
+  background-color: #276;
+  line-height: 50px;
+  color:red;
+}
 </style>
 
 
